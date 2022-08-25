94c71fb2eb886a1f6d8e45055da983e6718970a8
Nathan Burgess <nathans.email.address@gmail.com>
2022-08-25 11:17:11 -0700

    supabase gets comments, no display yet

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
 // importing other stuff, utility functions for:
 // working with supabase:
-import { checkAuth, signOutUser } from './fetch-utils.js';
+import { checkAuth, signOutUser, getUser, insertChat } from './fetch-utils.js';
 // pure rendering (data --> DOM):
 
 /*  "boiler plate" auth code */
 // checking if we have a user! (will redirect to auth if not):
 checkAuth();
 // can optionally return the user:
-// const user = checkAuth();
+const user = checkAuth();
 
 // sign out link:
 const signOutLink = document.getElementById('sign-out-link');
 signOutLink.addEventListener('click', signOutUser);
-/* end "boiler plate auth code" */
 
-// grab needed DOM elements on page:
+const chatInputEl = document.querySelector('#chat-input');
+const chatButtonEl = document.querySelector('#chat-button');
 
-// local state:
+chatButtonEl.addEventListener('click', async () => {
+    const chatValue = chatInputEl.value;
+    const chatEntry = { contents: chatValue, username: user.email };
+    const response = await insertChat(chatEntry);
 
-// display functions:
+});
 
-// events:
+console.log(user.email);
